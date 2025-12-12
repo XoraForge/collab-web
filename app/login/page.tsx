564bfc69382d693d19d202f47da118be55573686
Jian Wei <itsjianweii@gmail.com>
2025-12-12 16:15:56 +0800

    refactor: change logo to normal image w/o routes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import Logo from "@/components/common/Logo";
 import { Button } from "@/components/ui/button";
 import Icon from "@/components/ui/icon";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function page() {
@@ -9,7 +10,12 @@ export default function page() {
     <div className="h-screen w-full flex justify-center items-center">
       {/* Form */}
       <div className="h-[65%] w-[600px] bg-black flex flex-col justify-between shadow-gray-700 border-2 border-[#525252] rounded-md p-7">
-        <Logo />
+        <div className="flex items-center gap-x-3">
+          <div className="size-6 relative">
+            <Image src="/logo.png" alt="logo" fill className="object-contain" />
+          </div>
+          <h1 className="font-semibold">Collab</h1>
+        </div>
         <div className="flex flex-col self-center gap-y-5 w-[70%] h-full justify-center">
           <div className="flex flex-col gap-y-2">
             <h1 className="text-3xl font-semibold">Login into Collab</h1>
