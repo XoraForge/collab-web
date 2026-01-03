5d94c931524d47cd00c0117ef640fc5a122f38a0
Jian Wei <itsjianweii@gmail.com>
2026-01-04 05:42:39 +0800

    fix: add better eslint ruleset for husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,15 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
