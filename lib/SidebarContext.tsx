12016e95fc2a38d817483f4c6756708bc63db5c3
Jian Wei <itsjianweii@gmail.com>
2026-01-04 04:37:20 +0800

    feat: Add sidebar context

---
+++ lib/SidebarContext.tsx
@@ -0,0 +1,33 @@
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+export type SidebarType = {
+  isSidebarVisible: boolean;
+  setIsSidebarVisible: Dispatch<SetStateAction<boolean>>;
+};
+
+const SidebarContext = createContext<SidebarType | null>(null);
+
+export function useSidebarContext() {
+  if (!SidebarContext) throw new Error("Sidebar context is used outside!");
+  return useContext(SidebarContext);
+}
+
+export default function SidebarProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  return (
+    <SidebarContext.Provider value={{ isSidebarVisible, setIsSidebarVisible }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+}
