0b127335772761a53501f3b8401089af16e9e415
Jian Wei <itsjianweii@gmail.com>
2026-01-04 03:40:21 +0800

    refactor: add theme for navigation tab

--- components/header-tabs/NavigationTab.tsx
+++ components/header-tabs/NavigationTab.tsx
@@ -2,7 +2,7 @@ import { BookOpenText, X } from "lucide-react";
 
 export default function NavigationTab({ title }: { title: string }) {
   return (
-    <div className="flex gap-x-5 items-center w-fit px-3 py-2 rounded-tl-lg rounded-br-lg cursor-pointer bg-card border border-border hover:bg-border transition-colors duration-200">
+    <div className="flex gap-x-5 items-center w-fit px-3 py-2 rounded-tl-lg rounded-br-lg cursor-pointer bg-(--default-sidebar) border hover:bg-border transition-colors duration-200">
       <div className="flex gap-x-2 items-center">
         <div className="p-1 border border-border rounded-sm">
           <BookOpenText size={15} className="text-text)" />
