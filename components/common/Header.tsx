70ef38ac16b0349d228bcc23a76e26ef273c5886
Jian Wei <itsjianweii@gmail.com>
2026-01-04 03:39:28 +0800

    refactor: change navigation tab format

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="w-full flex items-center justify-between px-6 py-3 bg-card border-b border-border">
+    <div className="w-full flex items-center justify-between px-6 py-3 bg-(--default-sidebar) border border-border">
       <div className="flex w-full items-center gap-x-4">
         {/* Navigation */}
         <div className="flex gap-x-3 pr-5 border-r border-border text-text">
@@ -19,8 +19,8 @@ export default function Header() {
 
         {/* Tabs */}
         <div className="flex w-full gap-x-1">
-          <NavigationTab title="Business Analytics" />
-          <NavigationTab title="Artificial Intelligence" />
+          <NavigationTab title="[ICT374] Business Analytics" />
+          <NavigationTab title="[ICT352] Artificial Intelligence" />
 
           {/* Add new project button */}
           <div className="flex items-center w-fit p-2 cursor-pointer hover:bg-border rounded-sm">
